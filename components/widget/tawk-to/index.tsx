2e77e8f6808c9fb0bee690a837545f714ff21203
saddam <saddam.dermawan@gmail.com>
2023-11-01 10:26:22 +0700

    .

--- components/widget/tawk-to/index.tsx
+++ components/widget/tawk-to/index.tsx
@@ -1,4 +1,6 @@
 import { useRef } from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 export default function TawkTo() {
